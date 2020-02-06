01a51b38fee8f3df2cc620b79c2175d6298c3aae
otorrone <olli.olapade@hotmail.com>
2020-02-06 17:35:54 +0200

    Update Database.py

--- Database.py
+++ Database.py
@@ -1,3 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
 CREATE TABLE Persons (
 	id integer PRIMARY KEY AUTOINCREMENT,
 	username string,
@@ -5,7 +17,7 @@ CREATE TABLE Persons (
 	last_name string,
 	birthdate string,
 	description string
-);
+)
 
 CREATE TABLE Matches (
 	id integer PRIMARY KEY AUTOINCREMENT,
@@ -17,15 +29,15 @@ CREATE TABLE Matches (
 	player1_score float,
 	player2_score float,
 	comment string
-);
+)
 
 CREATE TABLE Games (
 	id integer PRIMARY KEY AUTOINCREMENT,
 	name string,
 	score_type integer
-);
+)
 
 CREATE TABLE Player (
 	id integer,
 	game string
-);
+)
