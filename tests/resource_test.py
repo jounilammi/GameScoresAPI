c0ec66ffb4bac30d39a113c5506ac288ec0c396a
otorrone <olli.olapade@hotmail.com>
2020-04-17 01:13:14 +0300

    Update resource_test.py

---
+++ tests/resource_test.py
@@ -0,0 +1,62 @@
+import json
+import os
+import pytest
+import tempfile
+import time
+from jsonschema import validate
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+from sqlalchemy.exc import IntegrityError, StatementError
+
+from ..gamescoresAPI import db, app
+from ..gamescoresAPI.api import api
+from ..gamescoresapi.resources.game import GameCollection, GameItem
+from ..gamescoresapi.resources.match import MatchCollection, MatchItem
+from ..gamescoresapi.resources.person import PersonCollection, PersonItem
+from ..gamescoresapi.models import Game, Match, Person
+import utils
+
+def _get_person(word="a"):
+    '''
+    person = Person(username="NickName",
+    first_name="Testy", last_name="Tester")
+    '''
+    return(Person(username=word, first_name="Testy", last_name="Tester"))
+
+
+def _get_match(p1_id, p2_id, game_id):
+    match = Match(
+        game=game_id,
+        player1_id=p1_id,
+        player2_id=p2_id,
+        player1_score=23,
+        player2_score=30,
+        comment="well played"
+    )
+    return(match)
+
+
+def _get_game(gamename="a"):
+    game = Game(name=gamename, score_type=1)
+    return(game)
+
+def _populate_db():
+    """
+    Pre-populate database with 2 matches, 2 players and 2 matches
+    """
+
+    person1 = _get_person("aaaa")
+    person2 = _get_person("bbbb")
+    game = _get_game("tennis")
+    game2 = _get_game("golf")
+    match = _get_match(p1_id=1, p2_id=2, game_id=1)
+    match = _get_match(p1_id=1, p2_id=2, game_id=2)
+
+    db_handle.session.add(person1)
+    db_handle.session.add(person2)
+    db_handle.session.add(game)
+    db_handle.session.add(game2)
+    db_handle.session.add(match)
+    db_handle.session.add(match2)
+
+    db_handle.session.commit()
