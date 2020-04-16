0a1671c7489b729380b20f2c97b864174f3f1b60
otorrone <olli.olapade@hotmail.com>
2020-04-17 01:25:21 +0300

    Update resource_test.py

--- tests/resource_test.py
+++ tests/resource_test.py
@@ -14,7 +14,7 @@ from ..gamescoresapi.resources.game import GameCollection, GameItem
 from ..gamescoresapi.resources.match import MatchCollection, MatchItem
 from ..gamescoresapi.resources.person import PersonCollection, PersonItem
 from ..gamescoresapi.models import Game, Match, Person
-import utils
+
 
 def _get_person(word="a"):
     '''
@@ -60,3 +60,47 @@ def _populate_db():
     db_handle.session.add(match2)
 
     db_handle.session.commit()
+
+def _check_namespace(client, response):
+    """
+    Checks that the "kyykka" namespace is found from the response body, and
+    that its "name" attribute is a URL that can be accessed.
+    """
+
+    ns_href = response["@namespaces"]["kyykka"]["name"]
+    resp = client.get(ns_href)
+    assert resp.status_code == 200
+
+def _check_control_get_method(ctrl, client, obj):
+    """
+    Checks a GET type control from a JSON object be it root document or an item
+    in a collection. Also checks that the URL of the control can be accessed.
+    """
+
+    href = obj["@controls"][ctrl]["href"]
+    resp = client.get(href)
+    assert resp.status_code == 200
+
+
+
+def _check_control_post_method_player(ctrl, client, obj):
+    """
+    Checks a POST type control from a JSON object be it root document or an item
+    in a collection. In addition to checking the "href" attribute, also checks
+    that method, encoding and schema can be found from the control. Also
+    validates a valid sensor against the schema of the control to ensure that
+    they match. Finally checks that using the control results in the correct
+    status code of 201.
+    """
+
+    ctrl_obj = obj["@controls"][ctrl]
+    href = ctrl_obj["href"]
+    method = ctrl_obj["method"].lower()
+    encoding = ctrl_obj["encoding"].lower()
+    schema = ctrl_obj["schema"]
+    assert method == "post"
+    assert encoding == "json"
+    body = _get_player_json()
+    validate(body, schema)
+    resp = client.post(href, json=body)
+    assert resp.status_code == 201
